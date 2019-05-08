5ee92ca641355bf44684a324f35fc612c743cae3
Olha Yelisieieva <oyeli@softserveinc.com>
2019-05-08 16:36:21 +0300

    Issue-19: Added MIME type validator with tests

--- src/main/java/com/github/sylvainlaurent/maven/swaggervalidator/semantic/validator/ValidationContext.java
+++ src/main/java/com/github/sylvainlaurent/maven/swaggervalidator/semantic/validator/ValidationContext.java
@@ -1,16 +1,21 @@
 package com.github.sylvainlaurent.maven.swaggervalidator.semantic.validator;
 
 import static org.apache.commons.collections4.MapUtils.emptyIfNull;
+import static org.apache.commons.lang3.reflect.FieldUtils.readDeclaredStaticField;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import com.github.sylvainlaurent.maven.swaggervalidator.semantic.VisitableParameterFactory;
 import com.github.sylvainlaurent.maven.swaggervalidator.semantic.node.VisitableParameter;
 import com.github.sylvainlaurent.maven.swaggervalidator.semantic.node.path.PathWrapper;
-
+import com.google.common.net.MediaType;
+import edu.emory.mathcs.backport.java.util.Arrays;
 import io.swagger.models.Model;
 import io.swagger.models.Path;
 import io.swagger.models.Swagger;
@@ -18,7 +23,7 @@ import io.swagger.models.auth.SecuritySchemeDefinition;
 import io.swagger.models.parameters.Parameter;
 
 public class ValidationContext {
-
+    private final Set<String> definedMimeTypes;
     protected List<PathWrapper> paths = new ArrayList<>();
     protected Map<String, Model> definitions = new HashMap<>();
     private Swagger swagger;
@@ -31,6 +36,7 @@ public class ValidationContext {
         this.swagger = swagger;
         setPaths(emptyIfNull(swagger.getPaths()));
         setDefinitions(emptyIfNull(swagger.getDefinitions()));
+        definedMimeTypes = getDefinedMimeTypes();
     }
 
     public Swagger getSwagger() {
@@ -65,5 +71,27 @@ public class ValidationContext {
 
     public Map<String, SecuritySchemeDefinition> getSecurityDefinitions() {
         return swagger.getSecurityDefinitions();
+    }
+
+    @SuppressWarnings("unchecked")
+    private Set<String> getDefinedMimeTypes() {
+        Set<String> set;
+        try {
+            set = ((Map<MediaType, MediaType>) readDeclaredStaticField(MediaType.class, "KNOWN_TYPES", true)).keySet().stream()
+                    .map(x -> x.withoutParameters().toString()).collect(Collectors.toSet());
+        } catch (IllegalAccessException e) {
+            set = new HashSet<>();
+        }
+        return set;
+    }
+
+    public Set<String> getMimeTypes() {
+        return definedMimeTypes;
+    }
+
+    public void addCustomMimeTypes(String[] customMimeTypes) {
+        if (customMimeTypes != null) {
+            definedMimeTypes.addAll(Arrays.asList(customMimeTypes));
+        }
     }
 }
